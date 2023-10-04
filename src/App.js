f66ddab4fb2d5c6753f16d709046d43e6cff6361
Raphael David A Villejo <raphael>
2023-10-03 22:19:43 -0600

    updating our directory page

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./categories.styles.scss"
+
+import CategoryContainer from "./components/categories/category-container/category-container.component"
+
+const App = () => {
+  return <CategoryContainer />
+}
+
+export default App
